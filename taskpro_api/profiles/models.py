f898335ad178765e358a6439192d4a7d37f8ab7b
kedi1991 <okurut.henry@yahoo.com>
2023-10-10 23:48:30 +0000

    Add a signal to the profile model and perform a migration

--- taskpro_api/profiles/models.py
+++ taskpro_api/profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -18,3 +19,11 @@ class Meta:
 
 def __str__(self):
     return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(owner=instance)
+
+
+post_save.connect(create_profile, sender=User)
