b3381ef944491f8954fedf64606e78be7f8c58e7
kedi1991 <okurut.henry@yahoo.com>
2023-10-11 22:26:20 +0000

    Add a serializer and view to the task app

---
+++ taskpro_api/tasks/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Task
+
+
+class TaskSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
+
+    class Meta:
+        model = Task
+        fields = [
+            'id', 'owner', 'created_at', 'updated_at', 'task_name', 'description', 'assignees', 'project', 'status', 'attachment', 'is_owner'
+        ]
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+ 
+
--- taskpro_api/tasks/views.py
+++ taskpro_api/tasks/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+from django.http import Http404
+from rest_framework import status
+from taskpro_api.permissions import IsOwnerOrReadOnly
 
-# Create your views here.
+class TaskList(APIView):
+    def get(self, request):
+        tasks = Task.objects.all()
+        serializer = TaskSerializer(tasks, many= True, context= {'request' : request})
+        return Response(serializer.data)
+
+
+class TaskDetail(APIView):
+
+    #Simplify the API view
+    serializer_class = TaskSerializer
+    permission_classes =[IsOwnerOrReadOnly]
+
+
+    def get_object(self, pk):
+        try:
+            task=Task.objects.get(pk=pk)
+            self.check_object_permissions (self.request, task)
+            return task
+        except Task.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        task = self.get_object(pk)
+        serializer=TaskSerializer(task, context= {'request' : request})
+        return Response(serializer.data)
+
+    def put (self, request, pk):
+        task =self.get_object(pk)
+        serializer=TaskSerializer(task, data=request.data, context= {'request' : request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
