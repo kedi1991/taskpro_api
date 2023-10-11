4c11a6eb19030ea94b46c1b35e32fb96bda74c57
kedi1991 <okurut.henry@yahoo.com>
2023-10-11 00:25:49 +0000

    Add a profile detail view to display detail information about a specific profile

--- taskpro_api/profiles/views.py
+++ taskpro_api/profiles/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
 from .serializers import ProfileSerializer
+from django.http import Http404
 
 
 class ProfileList(APIView):
@@ -10,6 +11,19 @@ class ProfileList(APIView):
         serializer = ProfileSerializer(profiles, many= True)
         return Response(serializer.data)
 
+
+class ProfileDetail(APIView):
+    def get_object(self, pk):
+        try:
+            profile=Profile.objects.get(pk=pk)
+            return profile
+        except Profile.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        profile = self.get_object(pk)
+        serializer=ProfileSerializer(profile)
+        return Response(serializer.data)
 
 
 
