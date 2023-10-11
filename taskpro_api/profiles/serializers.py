2b7c03acdbaf276f34a8ee65fffee39665c8fa32
kedi1991 <okurut.henry@yahoo.com>
2023-10-11 02:14:14 +0000

    add is_owner field to the serializer and enable authentication and authorization

--- taskpro_api/profiles/serializers.py
+++ taskpro_api/profiles/serializers.py
@@ -4,10 +4,17 @@ from .models import Profile
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    is_owner = serializers.SerializerMethodField()
+
 
     class Meta:
         model = Profile
         fields = [
-            'id', 'owner', 'created_at', 'updated_at', 'name', 'about', 'image'
+            'id', 'owner', 'created_at', 'updated_at', 'name', 'about', 'image', 'is_owner'
         ]
+
+    def get_is_owner(self, obj):
+        request = self.context['request']
+        return request.user == obj.owner
+ 
 
