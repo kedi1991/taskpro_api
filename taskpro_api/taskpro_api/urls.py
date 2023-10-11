cb0ee5e692bfb851d1ee4a22ec847281b0766f45
kedi1991 <okurut.henry@yahoo.com>
2023-10-11 20:40:34 +0000

    Add the project urls for the get and put methods

--- taskpro_api/taskpro_api/urls.py
+++ taskpro_api/taskpro_api/urls.py
@@ -19,7 +19,9 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('profiles.urls')),
-        path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', include('projects.urls')),
+
 
 
 
