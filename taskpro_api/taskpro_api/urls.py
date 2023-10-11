1407ab3cd15e194c774c7225e6e9ec5692c89d2e
kedi1991 <okurut.henry@yahoo.com>
2023-10-11 01:57:44 +0000

    Enable the login button

--- taskpro_api/taskpro_api/urls.py
+++ taskpro_api/taskpro_api/urls.py
@@ -18,7 +18,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('profiles.urls'))
+    path('', include('profiles.urls')),
+        path('api-auth/', include('rest_framework.urls')),
+
 
 
 ]
