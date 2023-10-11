fc17993f0c51708db37975bba7a152e8db4bec52
kedi1991 <okurut.henry@yahoo.com>
2023-10-11 23:44:09 +0000

    Add can_list_tasks fail test case

--- taskpro_api/tasks/tests.py
+++ taskpro_api/tasks/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import Task
 
-# Create your tests here.
+class TaskListViewTests(APITestCase):
+    def setUp(self):
+        # Run this before the test
+        User.objects.create_user(username='kedi', password='kedi')
+
+    def test_can_list_tasks(self):
+        kedi = User.objects.get(username='kedi')
+        Task.objects.create(owner=kedi, task_name='Test task')
+        response = self.client.get('/tasks/')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        print(response.data, len(response.data))
+
