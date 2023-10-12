543b0725c07e3e5b9d05751a65b8a574730dbc01
kedi1991 <okurut.henry@yahoo.com>
2023-10-12 22:51:38 +0000

    Add a Procfie with startup settings for Heroku

--- taskpro_api/taskpro_api/settings.py
+++ taskpro_api/taskpro_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import os
 
 if os.path.exists('env.py'):
@@ -128,6 +129,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    if 'DEV' in os.environ 
+    else dj_database_url.parse(os.environ.get('DATABASE_URL'))
+
 }
 
 
